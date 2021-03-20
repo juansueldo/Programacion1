6538b2361dde2caf6f4f22098e2dcb5cecefb859
juansueldo <juan.sueldo1989@gmail.com>
2021-03-20 15:46:42 -0300

    resolucion ejercios clase sabado

--- Programacion 1/Realizados en Dev C++/Repaso Sabados/Tarea2.c
+++ Programacion 1/Realizados en Dev C++/Repaso Sabados/Tarea2.c
@@ -13,26 +13,35 @@ int main ()
 	int maximo;
 	int minimo;
 	int i;
+	int cont = 1;
+	int numIterMax;
+	int numIterMin;
 	
 	printf("Ingrese un numero:\n");
 	scanf("%d",&numero);
 	maximo = numero;
 	minimo = numero;
+	numIterMax = cont;
+	numIterMin = cont;
+	
 	
 	for(i = 0; i<4; i++){
 		printf("Ingrese un numero:\n");
 		scanf("%d",&numero);
-		
+		cont ++;
 		if(numero > maximo)
 		{
 			maximo = numero;
+			numIterMax = cont;
+			
 		}
 		if(numero < minimo)
 		{
 			minimo = numero;
+			numIterMin = cont;
 		}
 	}
-	printf("El maximo es %d y el minimo es %d", maximo, minimo);
+	printf("El maximo es %d y se ingreso en el orden %d\nEl minimo es %d y se ungreso en el orden %d",maximo,numIterMax, minimo,numIterMin);
 	
-	return 0;
+	return EXIT_SUCCESS;
 }
